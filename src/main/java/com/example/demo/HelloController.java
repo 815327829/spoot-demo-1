77f504d780f11b3ea676ca5b478202d1d8d7fa09
siro <815327829@qq.com>
2023-12-26 22:40:38 +0800

    yml配置2种方法

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,12 +1,36 @@
 package com.example.demo;
 
-
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Value("${name}")
+    private String name;
+
+    @Value("${person.age}")
+    private int age;
+
+    @Value("${address[0]}")
+    private String name2;
+
+    @Autowired
+    private Environment env;
+
+    @RequestMapping("/hello2")
+    public String hello2(){
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(name2);
+        System.out.println("_____________________");
+        System.out.println(env.getProperty("person.age"));
+        return "hello";
+    }
+
 
     @RequestMapping("/hello")
     public String hello(){
